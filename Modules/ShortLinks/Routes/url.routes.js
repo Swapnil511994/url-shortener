1cdac69cbc81ac097b41dd49e0bd5ae3e5f8c3be
Swapnil511994 <Swapnil511994@gmail.com>
2024-04-28 20:21:17 +0530

    MModified routes to include validations

--- Modules/ShortLinks/Routes/url.routes.js
+++ Modules/ShortLinks/Routes/url.routes.js
@@ -1,12 +1,26 @@
 import express from "express";
 import UrlController from "../Controllers/url.controller.js";
 import { authenticateToken } from "../../Auth/Middleware/auth.middleware.js";
+import {
+  createUrlValidation,
+  updateUrlValidation,
+} from "../Validations/url.validation.js";
 
 const urlController = new UrlController();
 const router = express.Router();
 
-router.post("/url/add-url", authenticateToken, urlController.addUrl);
-router.post("/url/update-url", authenticateToken, urlController.updateUrl);
+router.post(
+  "/url/add-url",
+  createUrlValidation,
+  authenticateToken,
+  urlController.addUrl
+);
+router.post(
+  "/url/update-url",
+  updateUrlValidation,
+  authenticateToken,
+  urlController.updateUrl
+);
 router.get("/:short_code", urlController.getUrlByShortCode);
 router.post(
   "/url/get-urls-by-user",
