8841d73fbc32e079ecf7c9316fa2ee6081335a2d
Swapnil511994 <Swapnil511994@gmail.com>
2024-04-28 20:16:57 +0530

    Added Validator for URL Services

---
+++ Modules/ShortLinks/Validations/url.validation.js
@@ -0,0 +1,37 @@
+import Joi from "joi";
+
+const createUrlSchema = Joi.object({
+  title: Joi.string().required().max(100),
+  url: Joi.string().required().max(600),
+});
+
+const updateUrlSchema = Joi.object({
+  title: Joi.string().optional().max(100),
+  url: Joi.string().optional().max(600),
+  short_code: Joi.string().optional().max(11),
+  is_active: Joi.boolean().optional(),
+});
+
+export const createUrlValidation = (req, res, next) => {
+  const { error } = createUrlSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({
+      status: false,
+      message: "Invalid Request Body",
+      data: error?.message,
+    });
+  }
+  next();
+};
+
+export const updateUrlValidation = (req, res, next) => {
+  const { error } = updateUrlSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({
+      status: false,
+      message: "Invalid Request Body",
+      data: error?.message,
+    });
+  }
+  next();
+};
